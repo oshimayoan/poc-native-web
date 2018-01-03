6b335268b7e5a11d52a9097582ddebb035fdcf3d
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2018-01-03 14:29:19 +0700

    Add flow-typed

---
+++ flow-typed/global.js
@@ -0,0 +1,10 @@
+// @flow
+/* eslint-disable no-undef */
+
+import React from 'react';
+import type {StyleObj} from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
+
+declare type StyleSet = StyleObj | Object | Array<StyleObj | Object>;
+
+declare type ReactNode = React.Node;
+declare type ReactEl = React.Element<*>;
